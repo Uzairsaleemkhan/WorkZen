74ab647f0f55d8a3cf006db455fcd96e97cd8777
Uzair Saleem <uzairsaleemdev@gmail.com>
2024-01-30 17:55:42 +0500

    Added create project controller

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -12,19 +12,62 @@ const bcrypt = require("bcrypt");
 const projectController={
 
    createProject(req,res,next){
-        const{email,title,description}= req.body;
-        knex.select('*').from('users').where({email}).first()
-        .then(user=>{
-            if(user.role==='team member'){
-                throw new Error('unauthorized')
-            }
+        const{email,title,description,membersArray}= req.body;
+        knex.transaction(trx=>{
+
+
+
+            return trx('projects')
+           .insert({email,title,description})
+           .returning('*')
+           .first()
+
+
+
+           .then(project=>{
+                return trx('projectrole')
+                .insert({role:'project member',projectid:project.id,email})
+                .returning('*')
+                .first()
+            })
+
+
+
+           .then(projectrole=>{
+               return trx('projectmembers')
+               .insert({projectid:projectrole.projectid,email})
+               .returning('*')
+               .first()            
+            })
+
+
+            .then(projectmember=>{
+                
+                knex.select('*')
+                .from('users')
+                .whereIn('email',membersArray)
+                .then(users=>{
+                    users.forEach((user)=>{
+                        
+                    })
+                })
+            })
+
+
+           .then(trx.commit)
+           .catch(trx.rollback)
         })
+       .then(result=>{
+            console.log('result transaction success')
+
+        })
+       
    }
 
 
 }
 //      ---------------projects schema--------------
-//projects       ----id-----email----------status--------title--------description--------
+//projects       ----id-----email----------status--------title--------description-------
 //projectmembers ----id-----projectid------email------
 //projectrole    ----id-----role-----------projectid-----email
 
